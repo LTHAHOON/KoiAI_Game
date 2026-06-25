ba53f01867730138e6a5594743fc8a1659c578f4
HAHOON <dlxogns8997@gmail.com>
2026-06-26 01:55:18 +0900

    Fix HealthBar

---
+++ Assets/_Project/Assets/Scripts/Utilities/EntityUtils.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public static class EntityUtils
+{
+    /// <summary>
+    /// Entity ID 가져오기 
+    /// </summary>
+    public static ulong GetEntityULongID([CanBeNull] this Object obj)
+    {
+        if (!obj)
+        {
+            throw new System.ArgumentNullException(nameof(obj));
+        }
+        EntityId entityId = obj.GetEntityId();
+        ulong id = EntityId.ToULong(entityId);
+        return id;
+    }
+}
